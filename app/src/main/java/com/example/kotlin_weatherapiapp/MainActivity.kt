f100b3250fe49cb032a67ec1aedcdaad17b5f4d6
kwonjuyoung <20161527@vision.hoseo.edu>
2022-03-30 01:16:44 +0900

    demo

--- app/src/main/java/com/example/kotlin_weatherapiapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_weatherapiapp/MainActivity.kt
@@ -1,11 +1,103 @@
 package com.example.kotlin_weatherapiapp
+//REST API 에서 REST란 Representational state transfer의 줄임말로서 시스템이 텍스트 기반 데이터에 접근하고 저작할 수 있게 하면서 상태를 유지하지 않는 오퍼레이션에 정의하는 것.
+//HTTP 메소드에는 GET, HEAD, POST, PUT이 있다.
 
+
+import android.app.Dialog
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CallAPILoginAsyncTask().execute()
+    }
+
+
+    private inner class CallAPILoginAsyncTask() : AsyncTask<Any, Void, String>(){
+
+        private lateinit var customProgressDialog: Dialog
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg p0: Any?): String {
+            var result :String
+            var connection:HttpURLConnection?=null
+
+            try {
+                val url = URL("https://run.mocky.io/v3/d4856fdb-d6a5-4a31-a778-184e584aa0b3")
+                connection = url.openConnection() as HttpURLConnection
+                connection.doInput = true
+                connection.doOutput = true
+
+                val httpResult :Int = connection.responseCode
+
+                if(httpResult == HttpURLConnection.HTTP_OK){
+                    val inputStream = connection.inputStream
+
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val stringBuilder = StringBuilder()
+                    var line: String?
+                    try{
+                        while (reader.readLine().also{ line=it} !=null){
+                            stringBuilder.append(line+"\n")
+
+                        }
+                    }catch (e:IOException){
+                        e.printStackTrace()
+                    }finally {
+                        try {
+                            inputStream.close()
+                        }catch (e:IOException){
+                            e.printStackTrace()
+                        }
+                    }
+                    result = stringBuilder.toString()
+                }else{
+                    result = connection.responseMessage
+                }
+            }catch (e: SocketTimeoutException){
+                result = "Connection Timeout"
+            }catch (e:Exception){
+                result = "Error : " + e.message
+            }finally {
+                connection?.disconnect()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            cancelProgressDialog()
+
+           // Log.i("JSON RESPONSE RESULT", result)
+        }
+
+        private fun showProgressDialog(){
+            customProgressDialog = Dialog(this@MainActivity)
+            customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            customProgressDialog.show()
+        }
+
+        private fun cancelProgressDialog(){
+            customProgressDialog.dismiss()
+        }
     }
 }
